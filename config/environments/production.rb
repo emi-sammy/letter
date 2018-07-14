8b03df0b910ca19c3bdfd86e43bfc16f92d713cb
“oikawaemi” <emi-sammy@joy.ocn.ne>
2018-07-14 11:23:57 +0900

    任意のコメント

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'secret-basin-18373.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'secret-basin-18373' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
